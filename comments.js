6cf66445faeb02b3bf0e1ba3966af8e6ceb16346
Karunakar Neriyanuru <karunakar.neriyanuru@csgsol.com>
2024-07-26 02:12:58 +0000

    Changed the implementation in comments.js

--- comments.js
+++ comments.js
@@ -1,68 +1,49 @@
-// Create web server
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
+// Create web servers and listen to incoming requests
+// Load the HTTP module
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var querystring = require("querystring");
+var comments = [];
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-
-const commentsByPostId = {};
-
-app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
-});
-
-app.post('/posts/:id/comments', async (req, res) => {
-    const commentId = require('crypto').randomBytes(4).toString('hex');
-    const { content } = req.body;
-
-    const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content, status: 'pending' });
-    commentsByPostId[req.params.id] = comments;
-
-    await axios.post('http://event-bus-srv:4005/events', {
-        type: 'CommentCreated',
-        data: {
-            id: commentId,
-            content,
-            postId: req.params.id,
-            status: 'pending'
-        }
-    });
-
-    res.status(201).send(comments);
-});
-
-app.post('/events', async (req, res) => {
-    console.log('Event Received:', req.body.type);
-
-    const { type, data } = req.body;
-
-    if (type === 'CommentModerated') {
-        const { postId, id, status, content } = data;
-        const comments = commentsByPostId[postId];
-
-        const comment = comments.find(comment => {
-            return comment.id === id;
-        });
-        comment.status = status;
-
-        await axios.post('http://event-bus-srv:4005/events', {
-            type: 'CommentUpdated',
-            data: {
-                id,
-                status,
-                postId,
-                content
+var server = http.createServer(function (req, res) {
+    var parsedUrl = url.parse(req.url);
+    var pathName = parsedUrl.pathname;
+    var query = querystring.parse(parsedUrl.query);
+    if (pathName === "/") {
+        fs.readFile("./index.html", function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.writeHead(200, {
+                    "Content-Type": "text/html"
+                });
+                res.write(data);
+                res.end();
             }
         });
+    } else if (pathName === "/comment") {
+        comments.push(query.comment);
+        res.writeHead(200, {
+            "Content-Type": "text/plain"
+        });
+        res.write("Comment added");
+        res.end();
+    } else if (pathName === "/getComments") {
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        });
+        res.write(JSON.stringify(comments));
+        res.end();
+    } else {
+        res.writeHead(404, {
+            "Content-Type": "text/plain"
+        });
+        res.write("Page not found");
+        res.end();
     }
-
-    res.send({});
 });
 
-app.listen(4001, () => {
-    console.log('Listening on 4001');
+server.listen(3000, function () {
+    console.log("Server is running on port 3000");
 });
